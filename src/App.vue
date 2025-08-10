2c93336d9ce5601040059f8a1ff8f4cef747cf0e
wetuhutu <2460252306@qq.com>
2025-08-10 09:23:11 +0800

    V1.02

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,10 @@ import search from './views/search.vue';
         <search />
       </div>
       <div class="main-content">
+        <div class="updata">
+          <button class="content">更新文章</button>
+        </div>
+        
         <div class="card"><articleCard /></div>
         <div class="card"><articleCard /></div>
         <div class="card"><articleCard /></div>
@@ -54,6 +58,39 @@ main {
     height: 100vh;
 }
 
+.updata{
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 35px;
+  /* background-color: antiquewhite; */
+}
+
+.updata .content{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  margin-left: 30px;
+  width: 100px;
+  font-family: sans-serif;
+  font-size: 15px;
+  color: rgba(0, 255, 255, 0.5);
+  border-radius: 30px;
+  background-color: rgba(255, 255, 255, 0.1);
+  border: none;
+  outline: none;
+  backdrop-filter: blur(5px);
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+
+.updata .content:hover {
+  transform: scale(1.1);
+  background-color: rgba(255, 255, 255, 0.1);
+  transition: all 0.3s ease-in-out;
+}
+
 .header {
     height: 30%;
     display: flex;
@@ -79,6 +116,7 @@ main {
   align-items: center;
   justify-content: center;
   margin: 20px;
+  margin-top: 10px;
   height: 180px;
 }
 
